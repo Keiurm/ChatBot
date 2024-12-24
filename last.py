1e56cf3bd2995352ddf4f3a2f9c6a950505cd9f8
Keiurm <kimurakeisukej@gmail.com>
2024-12-24 12:52:49 +0000

    add: 新しいルートとテンプレートを追加し、スタイルシートを作成

--- last.py
+++ last.py
@@ -3,13 +3,62 @@ from redis import Redis
 
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host="redis", port=6379)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+    redis.incr("hits")
+    return "Hello World! I have been seen %s times!!!!" % redis.get("hits")
+
+
+@app.route("/t1")
+def t1():
+    return render_template("t1.html")
+
+
+@app.route("/t2", methods=["GET"])
+def t2():
+    name = request.args.get("name")
+    return render_template("t2.html", var1=name)
+
+
+@app.route("/t3", methods=["GET"])
+def t3():
+    return render_template("t3.html")
+
+
+@app.route("/get_post", methods=["GET"])
+def get_from_post():
+    return render_template("t4.html", msg="enter your name")
+
+
+@app.route("/get_post", methods=["POST"])
+def post_from_post():
+    name = request.form["myname"]
+    return render_template("t4.html", msg=f"hello {name}")
+
+
+@app.route("/t5")
+def t5():
+    return render_template("t5.html", msg="This is message", warning=True)
+
+
+@app.route("/t6")
+def t6():
+    data = ["Alice", "Bob", "Charlie", "David", "Eve"]
+    return render_template("t6.html", unrolled_data=data, data=data)
+
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "GET":
+        return render_template("chat.html", conversation="Hello")
+    elif request.method == "POST":
+        msg = request.form["msg"]
+        redis.lpush("log", msg)
+        message_log = redis.lrange("log", 0, -1)
+        return render_template("chat.html", conversation=message_log)
 
 
 if __name__ == "__main__":
