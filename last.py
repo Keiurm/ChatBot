17182ac62bbed9914d70d5a111610d89d158d581
Keisuke Kimura <kimurakeisukej@gmail.com>
2025-01-04 23:54:47 +0900

    add: OpenAI APIキーの取得機能を追加、チャット画面の追加

--- last.py
+++ last.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from redis import Redis
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
 
 app = Flask(__name__)
@@ -10,6 +13,11 @@ redis = Redis(host="redis", port=6379)
 def hello():
     redis.incr("hits")
     return render_template("chat.html", conversation="Hello")
+
+
+def get_openai_key():
+    load_dotenv()
+    return os.getenv("OPENAI_API_KEY")
 
 
 if __name__ == "__main__":
