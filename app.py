b34c1a1f965b96c18bbba0b1d784a52fd62fd05f
Keiurm <kimurakeisukej@gmail.com>
2025-01-10 23:41:24 +0000

    fix: メッセージ履歴を保存出来るように修正

--- app.py
+++ app.py
@@ -5,22 +5,41 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
-# openai.api_key = os.environ["OPENAI_API_KEY"]
 
 app = Flask(__name__)
 
 
 client = OpenAI()
 
+# 送信メッセージを保存するリスト
+message_list = []
+
+# 生成されたメッセージを保存するリスト
+response_list = []
+
 
 # 返信を生成
-def generate_response(message):
-    completion = client.chat.completions.create(
-        model="gpt-4o-mini",
-        store=True,
-        messages=[{"role": "user", "content": message}],
-    )
-    return completion.choices[0].message.content
+def generate_response(message, model):
+    if len(message_list) == 1:
+        completion = client.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": message}],
+        )
+        response_list.append(completion.choices[0].message.content)
+        return completion.choices[0].message.content
+    else:
+        messages = []
+        for user_msg, assistant_msg in zip(message_list, response_list):
+            messages.append({"role": "user", "content": user_msg})
+            messages.append({"role": "assistant", "content": assistant_msg})
+        messages.append({"role": "user", "content": message})
+
+        completion = client.chat.completions.create(
+            model=model,
+            messages=messages,
+        )
+        response_list.append(completion.choices[0].message.content)
+        return completion.choices[0].message.content
 
 
 @app.route("/")
@@ -36,8 +55,8 @@ def t1():
 @app.route("/response", methods=["POST"])
 def response():
     data = request.json
-    return jsonify(generate_response(data["message"]))
-    # return jsonify({"message": "Hello, World!"})
+    message_list.append(data["message"])
+    return jsonify(generate_response(data["message"], data["model"]))
 
 
 if __name__ == "__main__":
